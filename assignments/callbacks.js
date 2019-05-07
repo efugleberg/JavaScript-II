9e7094003bcfc373a77e546ad4eaaccef7c0ea06
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-05-07 15:15:14 -0500

    WIP: updated

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,29 +23,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+function test(abc) {
+  console.log(abc);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, test);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, test);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+sumNums(3, 4, test);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 }
+multiplyNums(2, 2, test);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
 
+contains('Gum', items, test);
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
